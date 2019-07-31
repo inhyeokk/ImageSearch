199df375083dd30d20e64a930e4f4517b0c8b679
rkddlsgur983 <rkddlsgur983@naver.com>
2019-07-31 17:56:17 +0900

    Initial commit

---
+++ app/src/main/java/com/rkddlsgur983/kakaopay/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rkddlsgur983.kakaopay
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
